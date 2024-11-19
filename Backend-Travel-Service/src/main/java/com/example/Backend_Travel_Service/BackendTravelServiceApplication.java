147ae166b72195c5c1b90a4a7cd802f17c5bf7a5
AndresPaucar7 <116117307+AndresPaucar7@users.noreply.github.com>
2024-11-19 12:20:18 -0500

    Add files via upload

---
+++ Backend-Travel-Service/src/main/java/com/example/Backend_Travel_Service/BackendTravelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Travel_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTravelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTravelServiceApplication.class, args);
+	}
+
+}
---
+++ Backend-Travel-Service/src/test/java/com/example/Backend_Travel_Service/BackendTravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Travel_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
